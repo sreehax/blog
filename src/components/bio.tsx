a5cb5f41257c96662f758a5e60fc7b1f2f7a9640
Sreehari Sreedev <sreeharisreedev1@gmail.com>
2021-09-16 21:10:42 -0700

    add back next/image

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -1,19 +1,19 @@
-// import Image from 'next/image'
-// import pfp from '~/assets/profile-pic.png'
+import Image from 'next/image'
+import pfp from '~/assets/profile-pic.png'
 import getConfig from 'next/config'
 const { publicRuntimeConfig: config } = getConfig();
 
 const Bio = () => {
     return (
         <div className="bio">
-            {/* <Image
+            <Image
                 src={pfp}
                 alt={config.author.name}
                 width={50}
                 height={50}
                 className="bio-avatar"
                 priority
-            /> */}
+            />
             <p>
             &nbsp;&nbsp;&nbsp;&nbsp;Written by <a href={`https://github.com/${config.social.github}`}><strong>{config.author.name}</strong></a>
             </p>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,18 @@
-// import Image from 'next/image'
-// import getConfig from 'next/config'
-// import pfp from '~/assets/profile-pic.png'
+import Image from 'next/image'
+import getConfig from 'next/config'
+import pfp from '~/assets/profile-pic.png'
 import Link from 'next/link'
 
-// const { publicRuntimeConfig: config } = getConfig();
+const { publicRuntimeConfig: config } = getConfig();
 
 const Footer = () => <div className="bio">
-    {/* <Image
+    <Image
         src={pfp}
         alt={config.author.name}
         className="bio-avatar"
         width="50"
         height="50"
-    /> */}
+    />
     <p>
         &nbsp;&nbsp;&nbsp;&nbsp;<strong><Link href="/">Return home</Link></strong>
     </p>
